b3df562c67f341806dd285dae39268072700d2a6
SandeepSingh1997 <sandeepsingh10101997@gmail.com>
2023-06-28 01:24:16 +0530

    added navigation to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,6 @@ import HomePage from "./pages/Home/Home";
 import VideoChatPage from "./pages/Videochat/VideoChatPage";
 import ErrorPage from "./pages/Error/ErrorPage";
 
-import auth from "./pages/Landing/firebaseAuth";
-import { onAuthStateChanged } from "firebase/auth";
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("user is logged in");
-  } else {
-    console.log("user is logged out");
-  }
-});
-
-onAuthStateChanged;
-
 function App() {
   return (
     <BrowserRouter>
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -2,9 +2,21 @@ import { useState } from "react";
 
 import Login from "../../components/Login/Login";
 import Signup from "../../components/Signup/Signup";
+import auth from "./firebaseAuth";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
   const [login, setLogin] = useState<Boolean>(true);
+
+  const user = auth.currentUser;
+
+  const navigate = useNavigate();
+
+  console.log(user);
+
+  if (user) {
+    navigate("/home");
+  }
 
   return (
     <main>
